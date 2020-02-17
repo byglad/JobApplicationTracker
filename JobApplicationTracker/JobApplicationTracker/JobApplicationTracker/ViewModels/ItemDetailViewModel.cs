d9236a3673a0de69a555a44a68ba91b8ea687e2c
byglad <byronglad@gmail.com>
2020-02-16 22:19:08 -0800

    New xamarin master detail template project

---
+++ JobApplicationTracker/JobApplicationTracker/JobApplicationTracker/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using JobApplicationTracker.Models;
+
+namespace JobApplicationTracker.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
